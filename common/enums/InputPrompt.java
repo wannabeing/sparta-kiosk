193f3b4e1835117db40619a8969012e15d7a98b7
wannabeing <wannabeing@naver.com>
2025-03-13 17:45:47 +0900

    chore: InputPrompt enum 클래스 ALL(전체비우기) 기능 추가 및 경로 이동

--- common/enums/InputPrompt.java
+++ common/enums/InputPrompt.java
@@ -1,8 +1,13 @@
-package io.output;
+package common.enums;
 
+/**
+ * OutputPrinter 에서
+ * 사용자 입력창을 동적으로 보여줄 때 사용하는 Enum 클래스
+ */
 public enum InputPrompt {
     EXIT("프로그램 종료: 0"),
     BACK("뒤로가기: 0"),
+    ALL("전체비우기: 0"),
     DEFAULT("");
 
     // ✅속성
